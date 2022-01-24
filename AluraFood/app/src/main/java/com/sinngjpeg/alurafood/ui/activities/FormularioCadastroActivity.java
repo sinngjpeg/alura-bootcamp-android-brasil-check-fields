d6e04d9f04a4ac6278a459d140e59d30e418aa56
unknown <Ingrid Silva>
2022-01-24 19:06:35 -0300

    implements first validate field

--- AluraFood/app/src/main/java/com/sinngjpeg/alurafood/ui/activities/FormularioCadastroActivity.java
+++ AluraFood/app/src/main/java/com/sinngjpeg/alurafood/ui/activities/FormularioCadastroActivity.java
@@ -3,7 +3,10 @@ package com.sinngjpeg.alurafood.ui.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.sinngjpeg.alurafood.R;
 
 public class FormularioCadastroActivity extends AppCompatActivity {
@@ -12,5 +15,12 @@ public class FormularioCadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_cadastro);
+
+        TextInputLayout textInputFullName = findViewById(R.id.edt_full_name);
+        EditText fieldFullName = textInputFullName.getEditText();
+        String fullName = fieldFullName.getText().toString();
+        if(fullName.isEmpty()){
+            fieldFullName.setError("Campo Obrigatorio");
+        }
     }
 }
