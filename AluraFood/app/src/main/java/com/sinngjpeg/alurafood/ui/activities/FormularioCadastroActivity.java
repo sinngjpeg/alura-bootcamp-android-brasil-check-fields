2ee060010778fee3bd125ae1efed16c6e38fec97
unknown <Ingrid Silva>
2022-01-24 20:37:09 -0300

    separate methods'

--- AluraFood/app/src/main/java/com/sinngjpeg/alurafood/ui/activities/FormularioCadastroActivity.java
+++ AluraFood/app/src/main/java/com/sinngjpeg/alurafood/ui/activities/FormularioCadastroActivity.java
@@ -9,8 +9,6 @@ import android.widget.EditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.sinngjpeg.alurafood.R;
 
-import org.w3c.dom.Text;
-
 public class FormularioCadastroActivity extends AppCompatActivity {
 
     @Override
@@ -18,21 +16,41 @@ public class FormularioCadastroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_cadastro);
 
-        TextInputLayout textInputFullName = findViewById(R.id.edt_full_name);
-        addValidateField(textInputFullName);
+        initializerFields();
 
-        TextInputLayout textInputCpf = findViewById(R.id.edt_cpf);
-        addValidateField(textInputCpf);
+    }
 
-        TextInputLayout textInputPhone = findViewById(R.id.edt_phone);
-        addValidateField(textInputPhone);
+    private void initializerFields() {
+        configureFieldFullName();
+        configureFieldCPF();
+        configureFieldPhone();
+        configureFieldEmail();
+        configureFieldPassword();
+    }
 
-        TextInputLayout textInputEmail = findViewById(R.id.edt_email);
-        addValidateField(textInputEmail);
-
+    private void configureFieldPassword() {
         TextInputLayout textInputPassword = findViewById(R.id.edt_password);
         addValidateField(textInputPassword);
+    }
 
+    private void configureFieldEmail() {
+        TextInputLayout textInputEmail = findViewById(R.id.edt_email);
+        addValidateField(textInputEmail);
+    }
+
+    private void configureFieldPhone() {
+        TextInputLayout textInputPhone = findViewById(R.id.edt_phone);
+        addValidateField(textInputPhone);
+    }
+
+    private void configureFieldCPF() {
+        TextInputLayout textInputCpf = findViewById(R.id.edt_cpf);
+        addValidateField(textInputCpf);
+    }
+
+    private void configureFieldFullName() {
+        TextInputLayout textInputFullName = findViewById(R.id.edt_full_name);
+        addValidateField(textInputFullName);
     }
 
     private void addValidateField(final TextInputLayout textInputField) {
@@ -42,14 +60,18 @@ public class FormularioCadastroActivity extends AppCompatActivity {
             public void onFocusChange(View view, boolean hasFocus) {
                 String text = field.getText().toString();
                 if (!hasFocus) {
-                    if (text.isEmpty()) {
-                        textInputField.setError("Campo Obrigatorio");
-                    } else {
-                        textInputField.setError(null);
-                        textInputField.setErrorEnabled(false);
-                    }
+                    validateMandatoryField(text, textInputField);
                 }
             }
         });
+    }
+
+    private void validateMandatoryField(String text, TextInputLayout textInputField) {
+        if (text.isEmpty()) {
+            textInputField.setError("Campo Obrigatorio");
+        } else {
+            textInputField.setError(null);
+            textInputField.setErrorEnabled(false);
+        }
     }
 }
