e87e3ed0a73249beb9bad04988d38e1f6ce2ed7c
unknown <Ingrid Silva>
2022-01-24 17:51:02 -0300

    implements formulario activity

---
+++ AluraFood/app/src/main/java/com/sinngjpeg/alurafood/ui/activities/FormularioCadastroActivity.java
@@ -0,0 +1,16 @@
+package com.sinngjpeg.alurafood.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sinngjpeg.alurafood.R;
+
+public class FormularioCadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_cadastro);
+    }
+}
