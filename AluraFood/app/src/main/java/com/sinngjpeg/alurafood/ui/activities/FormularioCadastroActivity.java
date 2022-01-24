eba5fc6f70ce657af5c43b5bcd251a561a2d8bc7
unknown <Ingrid Silva>
2022-01-24 19:27:54 -0300

    implements function to validate empty fields

--- AluraFood/app/src/main/java/com/sinngjpeg/alurafood/ui/activities/FormularioCadastroActivity.java
+++ AluraFood/app/src/main/java/com/sinngjpeg/alurafood/ui/activities/FormularioCadastroActivity.java
@@ -3,7 +3,7 @@ package com.sinngjpeg.alurafood.ui.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.Editable;
+import android.view.View;
 import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -17,10 +17,36 @@ public class FormularioCadastroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_formulario_cadastro);
 
         TextInputLayout textInputFullName = findViewById(R.id.edt_full_name);
-        EditText fieldFullName = textInputFullName.getEditText();
-        String fullName = fieldFullName.getText().toString();
-        if(fullName.isEmpty()){
-            fieldFullName.setError("Campo Obrigatorio");
-        }
+        final EditText fieldFullName = textInputFullName.getEditText();
+        addValidateField(fieldFullName);
+
+        TextInputLayout textInputCpf = findViewById(R.id.edt_cpf);
+        final EditText fieldCpf = textInputCpf.getEditText();
+        addValidateField(fieldCpf);
+
+        TextInputLayout textInputPhone = findViewById(R.id.edt_phone);
+        final EditText fieldPhone = textInputPhone.getEditText();
+        addValidateField(fieldPhone);
+
+        TextInputLayout textInputEmail = findViewById(R.id.edt_email);
+        final EditText fieldEmail = textInputEmail.getEditText();
+        addValidateField(fieldEmail);
+
+        TextInputLayout textInputPassword = findViewById(R.id.edt_password);
+        final EditText fieldPassword = textInputPassword.getEditText();
+        addValidateField(fieldPassword);
+
+    }
+
+    private void addValidateField(final EditText field) {
+        field.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                String text = field.getText().toString();
+                if (text.isEmpty()) {
+                    field.setError("Campo Obrigatorio");
+                }
+            }
+        });
     }
 }
