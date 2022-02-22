e51105e6685dd87fed6d10539d3aa1813ce06744
unknown <Ingrid Silva>
2022-02-22 19:27:01 -0300

    implements focus change listener to full name

--- AluraFoodValidateFields/app/src/main/java/com/sinngjpeg/alurafoodvalidatefields/MainActivity.kt
+++ AluraFoodValidateFields/app/src/main/java/com/sinngjpeg/alurafoodvalidatefields/MainActivity.kt
@@ -9,40 +9,39 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var txtFullName: TextInputEditText
     lateinit var txtFullNameLayout: TextInputLayout
+    lateinit var txtCPF: TextInputEditText
+    lateinit var txtCPFLayout: TextInputLayout
+    lateinit var txtPhoneNumber: TextInputEditText
+    lateinit var txtPhoneNumberLayout: TextInputLayout
+    lateinit var txtEmail: TextInputEditText
+    lateinit var txtEmailLayout: TextInputLayout
+    lateinit var txtPassword: TextInputEditText
+    lateinit var txtPasswordLayout: TextInputLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initializerFields()
-    }
+//        initializerFields()
 
-    private fun initializerFields() {
-        configureFullName()
-        configureCPF()
-        configurePhoneNumber()
-        configureEmail()
-        configurePassword()
-    }
-
-    fun configurePassword() {
-        TODO("Not yet implemented")
-    }
-
-    fun configureEmail() {
-        TODO("Not yet implemented")
-    }
-
-    fun configurePhoneNumber() {
-        TODO("Not yet implemented")
-    }
-
-    fun configureCPF() {
-        TODO("Not yet implemented")
-    }
-
-    fun configureFullName() {
         txtFullName = findViewById(R.id.edt_full_name)
         txtFullNameLayout = findViewById(R.id.edt_full_name_layout)
+        fullNameFocusListener()
+    }
+
+    fun fullNameFocusListener() {
+        txtFullName.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                txtFullNameLayout.helperText = validateFullName()
+            }
+        }
+    }
+
+    fun validateFullName(): String? {
+        val fullNameText = txtFullName.text.toString()
+        if (fullNameText.isEmpty()) {
+            return getString(R.string.full_name_empty)
+        }
+        return null
     }
 }
